ee22faebdc9f4de9e4343f6ebc5723b853d06f7c
Karel Smola <karelsmola21@gmail.com>
2023-04-21 10:27:52 +0200

    App apdetad by Max from Udemy. useFetcher added.

--- src/App.js
+++ src/App.js
@@ -1,80 +1,151 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./pages/Root";
-import HomePage from "./pages/HomePage";
-import EventsRoot from "./pages/EventsRoot";
-import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
+// import React from "react";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Root from "./pages/Root";
+// import HomePage from "./pages/HomePage";
+// import EventsRoot from "./pages/EventsRoot";
+// import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
+// import EventDetailPage, {
+//   loader as eventDetailLoader,
+//   action as deleteEvent,
+// } from "./pages/EventDetailPage";
+// import NewEventPage from "./pages/NewEventPage";
+// import EditEventPage from "./pages/EditEventPage";
+// import Error from "./pages/Error";
+// import { action as manipulateEvent } from "./components/EventForm";
+// // Challenge / Exercise
+
+// // 1. Add five new (dummy) page components (content can be simple <h1> elements)
+// //    - HomePage
+// //    - EventsPage
+// //    - EventDetailPage
+// //    - NewEventPage
+// //    - EditEventPage
+// // 2. Add routing & route definitions for these five pages
+// //    - / => HomePage
+// //    - /events => EventsPage
+// //    - /events/<some-id> => EventDetailPage
+// //    - /events/new => NewEventPage
+// //    - /events/<some-id>/edit => EditEventPage
+// // 3. Add a root layout that adds the <MainNavigation> component above all page components
+// // 4. Add properly working links to the MainNavigation
+// // 5. Ensure that the links in MainNavigation receive an "active" class when active
+// // 6. Output a list of dummy events to the EventsPage
+// //    Every list item should include a link to the respective EventDetailPage
+// // 7. Output the ID of the selected event on the EventDetailPage
+// // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+
+// const App = () => {
+//   const router = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <Root />,
+//       errorElement: <Error />,
+//       children: [
+//         { index: true, element: <HomePage /> },
+//         {
+//           path: "events",
+//           element: <EventsRoot />,
+//           children: [
+//             {
+//               index: true,
+//               element: <EventsPage />,
+//               loader: eventsLoader,
+//             },
+//             {
+//               path: ":eventId",
+//               id: "event-detail",
+//               loader: eventDetailLoader,
+//               children: [
+//                 {
+//                   index: true,
+//                   element: <EventDetailPage />,
+//                   action: deleteEvent,
+//                 },
+//                 {
+//                   path: "edit",
+//                   element: <EditEventPage />,
+//                   action: manipulateEvent,
+//                 },
+//               ],
+//             },
+//             { path: "new", element: <NewEventPage />, action: manipulateEvent },
+//           ],
+//         },
+//       ],
+//     },
+//   ]);
+//   return <RouterProvider router={router} />;
+// };
+
+// export default App;
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import EditEventPage from "./pages/EditEventPage";
+import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
-  action as deleteEvent,
+  action as deleteEventAction,
 } from "./pages/EventDetailPage";
+import EventsPage, { loader as eventsLoader } from "./pages/EventsPage";
+import EventsRoot from "./pages/EventsRoot";
+import HomePage from "./pages/HomePage";
 import NewEventPage from "./pages/NewEventPage";
-import EditEventPage from "./pages/EditEventPage";
-import Error from "./pages/Error";
-import { action as manipulateEvent } from "./components/EventForm";
-// Challenge / Exercise
+import Root from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-// 4. Add properly working links to the MainNavigation
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-// 7. Output the ID of the selected event on the EventDetailPage
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "events",
+        element: <EventsRoot />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ":eventId",
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: "newsletter",
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+    ],
+  },
+]);
 
-const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      errorElement: <Error />,
-      children: [
-        { index: true, element: <HomePage /> },
-        {
-          path: "events",
-          element: <EventsRoot />,
-          children: [
-            {
-              index: true,
-              element: <EventsPage />,
-              loader: eventsLoader,
-            },
-            {
-              path: ":eventId",
-              id: "event-detail",
-              loader: eventDetailLoader,
-              children: [
-                {
-                  index: true,
-                  element: <EventDetailPage />,
-                  action: deleteEvent,
-                },
-                {
-                  path: "edit",
-                  element: <EditEventPage />,
-                  action: manipulateEvent,
-                },
-              ],
-            },
-            { path: "new", element: <NewEventPage />, action: manipulateEvent },
-          ],
-        },
-      ],
-    },
-  ]);
+function App() {
   return <RouterProvider router={router} />;
-};
+}
 
 export default App;
--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,5 +1,44 @@
-import classes from "./MainNavigation.module.css";
+// import classes from "./MainNavigation.module.css";
+// import { NavLink } from "react-router-dom";
+
+// function MainNavigation() {
+//   return (
+//     <header className={classes.header}>
+//       <nav>
+//         <ul className={classes.list}>
+//           <li>
+//             <NavLink
+//               to="/"
+//               className={({ isActive }) =>
+//                 isActive ? classes.active : undefined
+//               }
+//               end
+//             >
+//               Home
+//             </NavLink>
+//           </li>
+//           <li>
+//             <NavLink
+//               to="/events"
+//               className={({ isActive }) =>
+//                 isActive ? classes.active : undefined
+//               }
+//             >
+//               Events
+//             </NavLink>
+//           </li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default MainNavigation;
+
 import { NavLink } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
 
 function MainNavigation() {
   return (
@@ -27,8 +66,19 @@ function MainNavigation() {
               Events
             </NavLink>
           </li>
+          <li>
+            <NavLink
+              to="/newsletter"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Newsletter
+            </NavLink>
+          </li>
         </ul>
       </nav>
+      <NewsletterSignup />
     </header>
   );
 }
